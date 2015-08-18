0472b58233408d431024fbff50e64ca44575e2f3
Nestor Garcia <nestor.garcia@tralix.com>
2015-08-18 18:17:42 -0500

    se dejan las líneas con la solución

--- src/multiplos35.clj
+++ src/multiplos35.clj
@@ -1,3 +1,4 @@
+; Suma los múltiplos de 3 y de 5 debajo de 1000
 (defn multiplo [n]
   (fn [x] (= (mod x n) 0)))
 
@@ -12,3 +13,7 @@
   (filter multiplo-valido? (range 1 limite)))
 
 (println (reduce + (multiplos 1000)))
+
+; en una línea de código
+; (reduce + (filter #(or (= (mod % 3) 0) (= (mod % 5) 0)) (range 1000)))
+
