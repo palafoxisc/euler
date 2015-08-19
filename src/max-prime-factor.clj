4b53d939271bb821a85b2feaae9a46a56d43d6c8
Nestor Garcia <nestor.garcia@tralix.com>
2015-08-19 09:16:35 -0500

    factor primo máximo de un número

--- src/max-prime-factor.clj
+++ src/max-prime-factor.clj
@@ -1,5 +1,13 @@
 ; Obtiene el factor primo mayor de un número
 
 (defn prime-factors
-  [n]
-  (loop 
+  ([n]
+   (prime-factors n 2 '()))
+  ([n candidate acc]
+   (cond (<= n 1)
+         (reverse acc)
+         (zero? (rem n candidate))
+         (recur (/ n candidate) candidate (cons candidate acc))
+         :else (recur n (inc candidate) acc))))
+
+(println (apply max (prime-factors 600851475143)))
