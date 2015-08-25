4876686717bb2b218b02484c0f58a0cbbb8b39b2
Nestor Garcia <nestor.garcia@tralix.com>
2015-08-24 19:16:47 -0500

    cambio de nombre de variables

--- src/prime-10001st.clj
+++ src/prime-10001st.clj
@@ -7,19 +7,16 @@
   (Math/round (+ (Math/pow index 2) (* multiplier index))))
 
 (defn primes
-  ([n array] (primes n array 2 (next-j 2 0) 1))
-  ([n array index j pow]
-;  (println "n" n "index" index "j" j "pow" pow "(sqrt-trunk n)" (sqrt-trunk n) "array" array)
-  (if (< index (sqrt-trunk n))
+  ([limit array] (primes limit array 2 4 1))
+  ([limit array index j multiplier]
+  (if (< index (sqrt-trunk limit))
     (if (nth array index)
-      (if (<  j n)
-        (recur n (assoc array j false) index (next-j index pow) (inc pow))
-        (recur n array (inc index) (next-j (inc index) 0) 1))
-      (recur n array (inc index) (next-j (inc index) 0) 1))
+      (if (<  j limit)
+        (recur limit (assoc array j false) index (next-j index multiplier) (inc multiplier))
+        (recur limit array (inc index) (next-j (inc index) 0) 1))
+      (recur limit array (inc index) (next-j (inc index) 0) 1))
       array)))
 
-(def lista (take 105000 (repeat true)))
+(def primes-list (take 105000 (repeat true)))
 
-;(println (map #(first %) (drop 2 (filter #(second %) (map-indexed vector (primes 39 (vec lista)))))))
-
-(println (nth (map #(first %) (drop 2 (filter #(second %) (map-indexed vector (primes 105000 (vec lista)))))) 10000))
+(println (nth (map #(first %) (drop 2 (filter #(second %) (map-indexed vector (primes 105000 (vec primes-list)))))) 10000))
