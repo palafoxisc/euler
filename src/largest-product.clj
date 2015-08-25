83515c58f86ab0509790234c6afb14b0e23f94e8
Nestor Garcia <nestor.garcia@tralix.com>
2015-08-25 13:28:17 -0500

    el producto mayor de una serie de números

---
+++ src/largest-product.clj
@@ -0,0 +1,19 @@
+(def content (slurp "numbers.txt"))
+
+(defn to-digits 
+  [string]
+  (map #(. Integer parseInt (. Character toString %))
+    (reduce concat (clojure.string/split string #"\n"))))
+
+(defn largest-product
+  ([n] (largest-product n (to-digits content) []))
+  ([n digits products]
+    (if (zero? (first digits))
+      (recur n (rest digits) products)
+      (if (not (= n (count (take n digits))))
+        products
+        (recur n (rest digits) (cons (reduce * (take n digits)) products))))))
+
+(println (apply max (largest-product 4)))
+
+;{:prod (reduce * (take 13 digits)) :digits (take 13 digits)}
--- src/prime-10001st.clj
+++ src/prime-10001st.clj
@@ -9,12 +9,12 @@
 (defn primes
   ([limit array] (primes limit array 2 4 1))
   ([limit array index j multiplier]
-  (if (< index (sqrt-trunk limit))
-    (if (nth array index)
-      (if (<  j limit)
-        (recur limit (assoc array j false) index (next-j index multiplier) (inc multiplier))
+    (if (< index (sqrt-trunk limit))
+      (if (nth array index)
+        (if (<  j limit)
+          (recur limit (assoc array j false) index (next-j index multiplier) (inc multiplier))
+          (recur limit array (inc index) (next-j (inc index) 0) 1))
         (recur limit array (inc index) (next-j (inc index) 0) 1))
-      (recur limit array (inc index) (next-j (inc index) 0) 1))
       array)))
 
 (def bool-array (take 105000 (repeat true)))
