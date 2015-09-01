b1dc042b5870e462c0e93671ebb6bb3a623bd0bd
Nestor Garcia <nestor.garcia@tralix.com>
2015-08-31 19:05:54 -0500

    solución al problema

--- src/largest-product-grid.clj
+++ src/largest-product-grid.clj
@@ -13,31 +13,38 @@
   [index digits]
   (take 4 (take-nth 20 (drop index digits))))
 
-
-(defn vertical2
+(defn diagonal
   [index digits]
   (take 4 (take-nth 21 (drop index digits))))
 
-(defn diagonal
-  [digits acc]
-    (if (zero? (count digits))
-      acc
-      (recur (drop 21 digits) (conj acc (first digits)))))
+(defn diagonal2
+  [index digits]
+  (take 4 (take-nth 19 (drop index digits))))
 
-; (defn largest-product
-;  ([n] (largest-product n (to-digits content) []))
-;  ([n digits products]
-;   (if (zero? (first digits))
-;    (recur n (rest digits) products)
-;    (if (not (= n (count (take n digits))))
-;     products
-;     (recur n (rest digits) (cons (reduce * (take n digits)) products))))))
+(defn to-map
+  [n arr]
+  {:n n :h {:product (reduce * (horizontal n arr)) :values (horizontal n arr)} :v {:product (reduce * (vertical n arr)) :values (vertical n arr)} :d {:product (reduce * (diagonal n arr)) :values (diagonal n arr)} :di {:product (reduce * (diagonal2 n arr)) :values (diagonal2 n arr)} })
+
+(defn largest-product
+  [n arr acc]
+;  (println "n" n "arr" arr "acc" acc)
+  (if (> n 400)
+    acc
+    (recur (inc n) arr (conj acc (to-map n arr)))))
 
 ;(println (count (reduce concat (map #(clojure.string/split % #" ") (clojure.string/split content #"\n")))))
-(println (digits content))
+;(println (digits content))
 ;(println (apply max (largest-product 13)))
-(println (horizontal 4 (digits content)))
-(println (vertical 0 (digits content)))
-(println (vertical 1 (digits content)))
-(println (diagonal (drop 1 (digits content)) []))
-(println (vertical2 19 (digits content)))
+;(println (horizontal 4 (digits content)))
+;(println (vertical 0 (digits content)))
+;(println (vertical 1 (digits content)))
+;(println (diagonal 3 (digits content)))
+;(println (diagonal2 7 (digits content)))
+
+;(println (largest-product 0 (digits content) []))
+;(println (map #(get-in % [:v]) (largest-product 0 (digits content) [])))
+(println (apply max (map #(get-in % [:h :product]) (largest-product 0 (digits content) []))))
+(println (apply max (map #(get-in % [:v :product]) (largest-product 0 (digits content) []))))
+(println (apply max (map #(get-in % [:d :product]) (largest-product 0 (digits content) []))))
+(println (apply max (map #(get-in % [:di :product]) (largest-product 0 (digits content) []))))
+;(println (apply max (largest-product 0 (digits content) [])))
