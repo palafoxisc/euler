870ac7deb9a01b56f274429a07b05a993aba3844
Nestor Garcia <nestor.garcia@tralix.com>
2015-09-03 17:08:33 -0500

    solución al problema

---
+++ src/highly-divisible-triangular-number.clj
@@ -0,0 +1,23 @@
+(defn check
+  [number index]
+  (if (not (= index (/ number index)))
+    [index (/ number index)]
+    [index]))
+
+(defn factors
+  ([number] (factors number 1 #{}))
+  ([number index acc]
+    (if (>= index (inc (Math/sqrt number)))
+      acc
+      (if (zero? (mod number index))
+        (recur number (inc index) (concat acc (check number index)))
+        (recur number (inc index) acc)))))
+
+(defn highly-div
+  ([] (highly-div 2))
+  ([start]
+    (if (> (count (factors (reduce + (range (inc start))))) 500)
+      start
+      (recur (inc start)))))
+
+(println (highly-div))
