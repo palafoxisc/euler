dce73888c4455479acbf8ffb80ec188909270f36
Nestor Garcia <nestor.garcia@tralix.com>
2015-09-22 17:49:53 -0500

    dos versiones de obtener los primeros 10 dígitos

--- src/large-sum.clj
+++ src/large-sum.clj
@@ -3,4 +3,6 @@
 (def numbers 
   (map #(bigint %) (clojure.string/split content #"\n")))
 
-(println (take 10 (str (reduce + numbers))))
+(println (subs (str (reduce + numbers)) 0 10))
+
+;(println (apply str (take 10 (str (reduce + numbers)))))
